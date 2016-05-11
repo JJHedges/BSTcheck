edd96e4063919b4e0bd55e9be7b9a7e20c353188
Jordan Hedges <jordanjameshedges3@gmail.com>
2016-05-11 19:43:59 +0800

    Updated algorithm to get rid of bug

--- ek.cpp
+++ ek.cpp
@@ -27,7 +27,6 @@ int bfs(int start, int end)
 	{
 		currentNode = q.front(); q.pop();
 		seen.insert(currentNode);
-		//std::cout << currentNode << std::endl;
 		
 		if(currentNode == end) break;	
 		
@@ -36,11 +35,10 @@ int bfs(int start, int end)
 			//If node has not been seen before and a path with non-zero remaining capacity
 			// exists push the node on and remember its parent	
 			if(seen.count(i) == 0 && capMatrix[currentNode][i] != -1 
-				&& capMatrix[currentNode][i] - flowMatrix[currentNode][i] != 0)
+				&& capMatrix[currentNode][i] - flowMatrix[currentNode][i] > 0)
 			{
 				q.push(i);
 				parentsList[i] = currentNode;
-				//std::cout << currentNode << " " << i << std::endl;
 			}							
 		}		
 	}
@@ -49,7 +47,6 @@ int bfs(int start, int end)
 	//Find the minimum amount of flow that can be pushed by retracing the path found
 	while(currentNode != start)
 	{
-		//std::cout << currentNode << std::endl;
 		int parent = parentsList[currentNode];
 		curPathCapacity = std::min(capMatrix[parent][currentNode] - 
 			flowMatrix[parent][currentNode], curPathCapacity);
@@ -61,6 +58,7 @@ int bfs(int start, int end)
 	{
 		int parent = parentsList[currentNode];
 		flowMatrix[parent][currentNode] += curPathCapacity;
+		flowMatrix[currentNode][parent] -= curPathCapacity;
 		currentNode = parent;
 	}
 	return curPathCapacity;	
@@ -73,7 +71,6 @@ int edmondsKarp(int source, int sink)
 	while(true)
 	{
 		int flow = bfs(source, sink);
-		//std::cout << "flow: " << flow << std::endl;
 		if(flow == 0)
 		{
 			break;
@@ -97,6 +94,18 @@ void test1()
 	capMatrix[4][1] = 1;
 	capMatrix[4][6] = 1;
 	capMatrix[5][6] = 9;
+	
+	capMatrix[1][0] = -3;
+	capMatrix[3][0] = -3;
+	capMatrix[2][1] = -4;
+	capMatrix[0][2] = -3;
+	capMatrix[3][2] = -1;
+	capMatrix[4][2] = -2;
+	capMatrix[4][3] = -2;
+	capMatrix[5][3] = -6;
+	capMatrix[1][4] = -1;
+	capMatrix[6][4] = -1;
+	capMatrix[6][5] = -9;
 		
 	flowMatrix[0][1] = 0;
 	flowMatrix[0][3] = 0;
@@ -109,6 +118,8 @@ void test1()
 	flowMatrix[4][1] = 0;
 	flowMatrix[4][6] = 0;
 	flowMatrix[5][6] = 0;
+	
+	
 	
 	int maxFlow = edmondsKarp(0, 6);
 	if(maxFlow == 5) std::cout << "correctly found flow" << std::endl;
