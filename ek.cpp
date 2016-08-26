2644275a5ee020f84cfd440a111a8f3eb0b80082
Jordan Hedges <jordanjameshedges3@gmail.com>
2016-08-26 19:52:39 +0800

    Was finding flow incorrectly, now resolved

--- ek.cpp
+++ ek.cpp
@@ -10,6 +10,8 @@
 std::vector<std::vector<int> > capMatrix(7, std::vector<int> (7, -1));
 //Adjacency matrix to hold current flow from i to j
 std::vector<std::vector<int> > flowMatrix(7, std::vector<int> (7, -1));
+//Residual matrix defined as capacity - flow for a edge (u,v) and flow for the reverse edge (v,u)
+std::vector<std::vector<int> > residualMatrix(7, std::vector<int> (7, -1));
 
 //BFS from start to end (source to sink) and return the flow of the augmenting path
 //if one can be found
@@ -35,12 +37,12 @@ int bfs(int start, int end)
 			//If node has not been seen before and a path with non-zero remaining capacity
 			// exists push the node on and remember its parent	
 			if(seen.count(i) == 0 && capMatrix[currentNode][i] != -1 
-				&& capMatrix[currentNode][i] - flowMatrix[currentNode][i] > 0)
+				&& residualMatrix[currentNode][i] != 0)
 			{
 				q.push(i);
 				parentsList[i] = currentNode;
-			}							
-		}		
+			}
+		}
 	}
 	
 	if(currentNode != end) return 0;
@@ -48,17 +50,17 @@ int bfs(int start, int end)
 	while(currentNode != start)
 	{
 		int parent = parentsList[currentNode];
-		curPathCapacity = std::min(capMatrix[parent][currentNode] - 
-			flowMatrix[parent][currentNode], curPathCapacity);
+		curPathCapacity = std::min(residualMatrix[parent][currentNode], curPathCapacity);
 		currentNode = parent;
 	}
 	currentNode = end;
-	//Push the flow by updating the flow matrix and return the amount pushed
+	//Push the flow by updating the flow matrix and the residual matrix and return the amount pushed
 	while(currentNode != start)
 	{
 		int parent = parentsList[currentNode];
 		flowMatrix[parent][currentNode] += curPathCapacity;
-		flowMatrix[currentNode][parent] -= curPathCapacity;
+        residualMatrix[parent][currentNode] = capMatrix[parent][currentNode] - flowMatrix[parent][currentNode];
+        residualMatrix[currentNode][parent] = flowMatrix[parent][currentNode];
 		currentNode = parent;
 	}
 	return curPathCapacity;	
@@ -95,17 +97,17 @@ void test1()
 	capMatrix[4][6] = 1;
 	capMatrix[5][6] = 9;
 	
-	capMatrix[1][0] = -3;
-	capMatrix[3][0] = -3;
-	capMatrix[2][1] = -4;
-	capMatrix[0][2] = -3;
-	capMatrix[3][2] = -1;
-	capMatrix[4][2] = -2;
-	capMatrix[4][3] = -2;
-	capMatrix[5][3] = -6;
-	capMatrix[1][4] = -1;
-	capMatrix[6][4] = -1;
-	capMatrix[6][5] = -9;
+	capMatrix[1][0] = 3;
+	capMatrix[3][0] = 3;
+	capMatrix[2][1] = 4;
+	capMatrix[0][2] = 3;
+	capMatrix[3][2] = 1;
+	capMatrix[4][2] = 2;
+	capMatrix[4][3] = 2;
+	capMatrix[5][3] = 6;
+	capMatrix[1][4] = 1;
+	capMatrix[6][4] = 1;
+	capMatrix[6][5] = 9;
 		
 	flowMatrix[0][1] = 0;
 	flowMatrix[0][3] = 0;
@@ -119,10 +121,45 @@ void test1()
 	flowMatrix[4][6] = 0;
 	flowMatrix[5][6] = 0;
 	
+	flowMatrix[1][0] = 0;
+	flowMatrix[3][0] = 0;
+	flowMatrix[2][1] = 0;
+	flowMatrix[0][2] = 0;
+	flowMatrix[3][2] = 0;
+	flowMatrix[4][2] = 0;
+	flowMatrix[4][3] = 0;
+	flowMatrix[5][3] = 0;
+	flowMatrix[1][4] = 0;
+	flowMatrix[6][4] = 0;
+	flowMatrix[6][5] = 0;
 	
+    residualMatrix[0][1] = capMatrix[0][1] - flowMatrix[0][1];
+	residualMatrix[0][3] = capMatrix[0][3] - flowMatrix[0][3];
+	residualMatrix[1][2] = capMatrix[1][2] - flowMatrix[1][2];
+	residualMatrix[2][0] = capMatrix[2][0] - flowMatrix[2][0];
+	residualMatrix[2][3] = capMatrix[2][3] - flowMatrix[2][3];
+	residualMatrix[2][4] = capMatrix[2][4] - flowMatrix[2][4];
+	residualMatrix[3][4] = capMatrix[3][4] - flowMatrix[3][4];
+	residualMatrix[3][5] = capMatrix[3][5] - flowMatrix[3][5];
+	residualMatrix[4][1] = capMatrix[4][1] - flowMatrix[4][1];
+	residualMatrix[4][6] = capMatrix[4][6] - flowMatrix[4][6];
+	residualMatrix[5][6] = capMatrix[5][6] - flowMatrix[5][6];
 	
+	residualMatrix[1][0] = flowMatrix[0][1];
+	residualMatrix[3][0] = flowMatrix[0][3];
+	residualMatrix[2][1] = flowMatrix[1][2];
+	residualMatrix[0][2] = flowMatrix[2][0];
+	residualMatrix[3][2] = flowMatrix[2][3];
+	residualMatrix[4][2] = flowMatrix[2][4];
+	residualMatrix[4][3] = flowMatrix[3][4];
+	residualMatrix[5][3] = flowMatrix[3][5];
+	residualMatrix[1][4] = flowMatrix[4][1];
+	residualMatrix[6][4] = flowMatrix[4][6];
+	residualMatrix[6][5] = flowMatrix[5][6];
+    
 	int maxFlow = edmondsKarp(0, 6);
-	if(maxFlow == 5) std::cout << "correctly found flow" << std::endl;
+    if(maxFlow == 5)
+        std::cout << "Found max flow of " << maxFlow << " correctly" << std::endl;
 }
 
 int main(int argc, char **argv)
